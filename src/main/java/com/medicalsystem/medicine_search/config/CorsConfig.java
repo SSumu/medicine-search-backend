6afb44400b103efc32d7a8e721f4ce0889296113
SSumu <sadeepalsumudu@gmail.com>
2026-03-28 03:44:59 +0530

    Swagger link was added to the CorsConfig.java

--- src/main/java/com/medicalsystem/medicine_search/config/CorsConfig.java
+++ src/main/java/com/medicalsystem/medicine_search/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**") // allow all endpoints
-                        .allowedOrigins("http://localhost:4200", "http://localhost:8080") // Allow Angular frontend and Swagger
+                        .allowedOrigins("http://localhost:4200", "http://localhost:8080/api/swagger-ui/index.html") // Allow Angular frontend and Swagger
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
 //                        .allowCredentials(true)
